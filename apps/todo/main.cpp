1a662235b489c8f13a61f63e0b19b7d6fad301e1
DMen6L <240103073@sdu.edu.kz>
2026-04-20 17:06:34 +0500

    feat: adding input fields and buttons test

--- apps/todo/main.cpp
+++ apps/todo/main.cpp
@@ -1,14 +1,42 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <string>
+#include <iostream>
 
 int main (int argc, char *argv[]) {
-  auto screen = ftxui::ScreenInteractive::TerminalOutput();
+  using namespace ftxui;
+  
+  std::string field;
+  std::string sbm;
 
-  auto button = ftxui::Button("Exit", [&]{
-      screen.Exit();
+  auto input = Input(&field, "Write something");
+
+  // Output Screen
+  auto screen = ScreenInteractive::Fullscreen();
+
+  // Button
+  auto btn = Button("Submit", [&] {
+      sbm = "Typed: " + field;
+      field = "";
   });
 
-  screen.Loop(button);
+  auto exit_btn = Button("Exit", screen.ExitLoopClosure());
 
+  //Base layout
+  auto layout = Container::Vertical({ input, btn, exit_btn });
+  
+  // Renderer
+  auto renderer = Renderer(layout, [&] {
+      return vbox({
+          text("Welcome!") | bold,
+          separator(),
+          input->Render(),
+          btn->Render(),
+          exit_btn->Render(),
+          text(sbm),
+      }) | border;
+  });
+
+  screen.Loop(renderer);
   return 0;
 }
